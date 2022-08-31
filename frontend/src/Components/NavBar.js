7951311087e0b0ba162801037b31eda06f05c60c
aidingdongg <gakramaqil@gmail.com>
2022-08-31 23:15:54 +0800

    Navbar

---
+++ frontend/src/Components/NavBar.js
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+const Links = ['Create an Itinerary', 'Itineraries', 'Bookings', 'Reviews', 'Sign In'];
+
