15d5d5d73755e7ece984fe71b7a232cd8245ddb9
Limmy97 <57515126+Limmy97@users.noreply.github.com>
2022-09-12 23:34:45 +0800

    Beta - Created sections, changed overall design
    Reformatted some code, changed landing page design to integrate with sign in

--- frontend/src/Components/SignIn.js
+++ frontend/src/Components/SignIn.js
@@ -1,48 +1,101 @@
-import { useState } from 'react';
+import {useState} from 'react';
 //import { useAuth } from '../lib/auth';
-import { FormControl, FormLabel, Button, Input, Box, Flex } from '@chakra-ui/react';
+import {
+    FormControl,
+    FormLabel,
+    Button,
+    Input,
+    Box,
+    Flex,
+    Grid,
+    GridItem,
+    Wrap,
+    WrapItem,
+    Center
+} from '@chakra-ui/react';
+
+const fields_width = '52.5%';
 
 const SignIn = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  //const { signIn } = useAuth();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  function onSubmit(e) {
-    e.preventDefault();
-    //signIn({ username, password });
-  }
+    //const { signIn } = useAuth();
 
-  return (
-    <Box>
-      <FormControl b={'1px'} id="signin">
-        <Flex
-        wrap = "wrap"
-        direction = "column"
-        align = "center"
+    function onSubmit(e) {
+        e.preventDefault();
+        //signIn({ username, password });
+    }
+
+    return (
+        <Grid
+            templateAreas={`"left_top right"
+                            "left_bottom right"
+                            `}
+            gridTemplateRows={'45fr 55fr'}
+            gridTemplateColumns={'3fr 2fr'}
+            h='600px'
+            // gap='1'
+            // color='blackAlpha.700'
+            fontWeight='bold'
         >
-          <FormLabel m={4}>Sign In</FormLabel>
-          <Input
-            m={4}
-            w = "40%"
-            type="text"
-            placeholder="username"
-            onChange={(e) => setUsername(e.target.value)}
-          ></Input>
-          <Input
-            m={4}
-            w = "40%"
-            type="password"
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-          ></Input>
-          <Button w={'40%'} m={4} type="submit" onClick={onSubmit} bg = "blue.75">
-          Log In
-          </Button>
-        </Flex>
-        
-      </FormControl>
-    </Box>
-  );
+            <GridItem pl='2' bg='blackAlpha.300' area={'left_top'}>
+                <Box position={'relative'} top={'50%'} left={'50%'} transform={'translate(-50%,-50%)'}
+                     textAlign={"center"}>
+                    <h1>
+                        <font size={6}>
+                            Welcome to TravelMaster
+                        </font>
+                    </h1>
+                    <h2>
+                        <font size={5}>
+                            Generate new itineraries at your fingertips.
+                        </font>
+                    </h2>
+                </Box>
+            </GridItem>
+            <GridItem pl='2' bg='white' area={'left_bottom'}>
+
+            </GridItem>
+            <GridItem pl='2' bg='cornflowerblue' area={'right'}>
+                <Box position={'relative'} top={'50%'} left={'50%'} transform={'translate(-50%,-50%)'}
+                     textAlign={"center"}>
+                    <FormControl b={'1px'} id="signin">
+                        <Flex
+                            wrap="wrap"
+                            direction="column"
+                            align="center"
+                        >
+                            <FormLabel m={4}>
+                                <font size={6}>
+                                    Log in to TravelMaster
+                                </font>
+                            </FormLabel>
+                            <Input
+                                m={4}
+                                w={fields_width}
+                                bgColor={'whitesmoke'}
+                                type="text"
+                                placeholder="username"
+                                onChange={(e) => setUsername(e.target.value)}
+                            ></Input>
+                            <Input
+                                m={4}
+                                w={fields_width}
+                                bgColor={'whitesmoke'}
+                                type="password"
+                                placeholder="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            ></Input>
+                            <Button w={fields_width} m={4} type="submit" onClick={onSubmit} bg="teal">
+                                <font size={5} color={'white'}>Log In</font>
+                            </Button>
+                        </Flex>
+                    </FormControl>
+                </Box>
+            </GridItem>
+        </Grid>
+    );
 };
 
 export default SignIn;
