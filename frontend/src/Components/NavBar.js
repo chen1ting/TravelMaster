d40a2473b522f6134f6b560099536f7de96b2f63
aidingdongg <gakramaqil@gmail.com>
2022-09-21 18:29:02 +0800

    ok

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,16 +1,15 @@
-
 import { useState } from 'react';
 import { Flex, Box, Text, useColorModeValue, Link, Menu, MenuButton, Button, Avatar, MenuList} from '@chakra-ui/react';
+
 
 const MenuItem = ({ children, to = '/'}) => {
   return (
     <Link 
-      px = "2"
+      px = "1"
       py = "1"
       rounded = "md"
       _hover = {{
-        bg: useColorModeValue('blue.100', 'blue.700'),
-        fontWeight: "bold",
+        bg: useColorModeValue('blue.300', 'blue.400'),
       }}
       href={to}>
       {children}
@@ -73,6 +72,7 @@ const Header = (props) => {
             </Box>
         </Flex>
     );
+
 
 
 };
