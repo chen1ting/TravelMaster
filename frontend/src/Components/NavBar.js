ce509d5436b72dbcb5a6cf0fb6eb7652bd9e7208
Limmy97 <57515126+Limmy97@users.noreply.github.com>
2022-09-25 13:54:15 +0800

    Revert "Merge branch 'main' into Profile"
    This reverts commit 5829065dbc77855c5dc9568771a45dd034d9caf7, reversing
    changes made to 2462a27b8513d09ff78144199e5b68de294b4c0e.

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -12,7 +12,6 @@ const MenuItem = ({ children, to = '/'}) => {
             rounded = "md"
             _hover = {{
                 bg: useColorModeValue('blue.300', 'blue.400'),
-                fontWeight: "bold",
             }}
             href={to}>
             {children}
@@ -44,11 +43,11 @@ const Header = (props) => {
             // mb={4}
             p={2}
             as="nav"
-            alignItems="center"
+            align="center"
             justify="space-between"
+            wrap="wrap"
             w="100%"
             bg="cornflowerblue"
-            h="6vh"
         >
             <Box w="200px" p="-1" paddingLeft={paddingSpace}>
                 <Text as="i" fontSize="5xl" fontWeight="bold">
@@ -61,7 +60,7 @@ const Header = (props) => {
                 flexBasis={{base: '100%', md: 'auto'}}
                 paddingRight={paddingSpace}
             >
-                <Flex fontSize={'xl'} px="10"
+                <Flex fontSize={'xl'}
                 >
                     <MenuItem to="/discover">
                         {MenuTextAlign("Discover")}
@@ -75,7 +74,7 @@ const Header = (props) => {
                     <MenuItem to="/bookings">
                         {MenuTextAlign("Bookings")}
                     </MenuItem>
-                    <Flex alignItems={'center'} px="3">
+                    <Flex alignItems={'center'}>
                         <Menu>
                             <MenuButton
                                 as={Button}
@@ -92,7 +91,7 @@ const Header = (props) => {
                                     }
                                 />
                             </MenuButton>
-                            <MenuList justify="center">
+                            <MenuList>
                                 <Box position={'relative'}
                                      top={'50%'}
                                      left={'50%'}
