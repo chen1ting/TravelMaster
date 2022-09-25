bc5c37e85d39ace73b6b9c9b8d7590343990c2ac
Limmy97 <57515126+Limmy97@users.noreply.github.com>
2022-09-25 14:37:30 +0800

    Revert "Revert "Merge branch 'main' into Profile""
    This reverts commit ce509d5436b72dbcb5a6cf0fb6eb7652bd9e7208.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import NavBar from './Components/NavBar';
-import {ChakraProvider} from '@chakra-ui/react';
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import {ChakraProvider} from "@chakra-ui/react";
 import {
     BrowserRouter as Router,
     Routes,
     Route,
-    Link as RouteLink
+    Link as RouteLink,
 } from "react-router-dom";
-import SignIn from './Components/SignIn';
+import SignIn from "./Components/SignIn";
+import Welcome from "./Components/Welcome";
 import SignUp from "./Components/SignUp";
 import Discover from "./Pages/Discover";
 import Reviews from "./Pages/Reviews";
 import ActivityDescription from './Pages/ActivityDescription';
 import Profile from "./Pages/Profile";
-
 
 function App() {
     return (
@@ -21,30 +21,13 @@ function App() {
             <Router>
                 <NavBar/>
                 <Routes>
-                    <Route
-                        path="discover"
-                        element={<Discover/>}
-                    />
-                    <Route
-                        path="reviews"
-                        element={<Reviews/>}
-                    />
-                    <Route
-                        path="signup"
-                        element={<SignUp/>}
-                    />
-                    <Route
-                        path="activitydescription"
-                        element={<ActivityDescription/>}
-                    />
-                    <Route
-                        path="profile"
-                        element={<Profile/>}
-                    />
-                    <Route
-                        path=""
-                        element={<SignIn/>}
-                    />
+                    <Route path="discover" element={<Discover/>}/>
+                    <Route path="reviews" element={<Reviews/>}/>
+                    <Route path="signup" element={<SignUp/>}/>
+                    <Route path="welcome" element={<Welcome/>}/>
+                    <Route path="activitydescription" element={<ActivityDescription/>}/>
+                    <Route path="profile" element={<Profile/>}/>
+                    <Route path="" element={<SignIn/>}/>
                 </Routes>
             </Router>
         </ChakraProvider>
