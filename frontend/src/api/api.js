c6ae94a1747d9bfd66e8ba229b1dc0f4eee0060a
tabbycat01 <geraldywy@gmail.com>
2022-09-27 20:13:37 +0800

    hookup login + mask password

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,7 +4,6 @@ import sha256 from "crypto-js/sha256";
 const ENDPOINT = "http://localhost:8080";
 
 const sendSignupReq = async (user, pass, email) => {
-  console.log("?");
   const rawResponse = await fetch(ENDPOINT + "/signup", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -45,4 +44,22 @@ const validateToken = async (tokenStr) => {
   return content.valid;
 };
 
-export { sendSignupReq, validateToken };
+const sendLoginReq = async (user, pass) => {
+  const rawResponse = await fetch(ENDPOINT + "/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: user,
+      hashed_password: sha256(pass).toString(),
+    }),
+  });
+
+  if (rawResponse.status !== 200) {
+    console.log("resp: " + rawResponse.status); // TODO: might wanna return an err message to display here
+    return null;
+  }
+  const content = await rawResponse.json();
+  return content;
+};
+
+export { sendSignupReq, validateToken, sendLoginReq };
