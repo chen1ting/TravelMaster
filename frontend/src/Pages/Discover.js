b40390a7063160f8bd6b52e00df62c774aee0efa
Limmy97 <57515126+Limmy97@users.noreply.github.com>
2022-10-02 19:18:17 +0800

    Discover page and Activity Card touch up (#46)
    * Revert Discover, Optimised Activity Cards
    
    Other Discover components partially done just kept for possible future use.
    
    * Discover and Activity stuff

--- frontend/src/Pages/Discover.js
+++ frontend/src/Pages/Discover.js
@@ -1,16 +1,19 @@
 import {
-    Box,
+    Box, Button,
     Grid,
     GridItem,
     Text
 } from "@chakra-ui/react";
 import DiscoverActivities from "./DiscoverSubPages/DiscoverActivities";
+import {useNavigate} from "react-router-dom";
 
 const Discover = () => {
+    const navigate = useNavigate();
+
     return (
         <Grid
-            templateAreas={`"title"
-                            "content"
+            templateAreas={`"title createNew"
+                            "content content"
                             `}
             gridTemplateRows={'10fr 90fr'}
             h='100vh'
@@ -18,9 +21,27 @@ const Discover = () => {
             bgColor={'blue.50'}
         >
             <GridItem area={"title"}>
-                <Box position={'relative'} top={'23%'} left={'7.5%'} transform={'translate(-50%,-50%)'}
+                <Box position={'relative'} top={'40%'} left={'17.5%'} transform={'translate(-50%,-50%)'}
                      textAlign={"left"} width='fit-content' mt={'1%'}>
                     <Text fontSize='4xl'>Discover</Text>
+                </Box>
+            </GridItem>
+            <GridItem area={'createNew'}>
+                <Box position={'relative'} top={'40%'} left={'80%'} transform={'translate(-50%,-50%)'}
+                     textAlign={"left"} width='fit-content' mt={'1%'}>
+                    <Button
+                        as="a"
+                        onClick={() => {
+                            navigate("/createevent");
+                        }}
+                        w={250}
+                        m={4}
+                        bg="teal"
+                    >
+                        <font size={5} color={"white"}>
+                            Create New Activity
+                        </font>
+                    </Button>
                 </Box>
             </GridItem>
             <GridItem area={"content"} bgColor={'white'}>
