e00e91dc2c8ec205375421d761a990cc8a661c8f
jmytkh <79437616+jmytkh@users.noreply.github.com>
2022-10-02 16:47:50 +0800

    Update DiscoverActivities.js

--- frontend/src/Pages/DiscoverSubPages/DiscoverActivities.js
+++ frontend/src/Pages/DiscoverSubPages/DiscoverActivities.js
@@ -45,21 +45,6 @@ const DiscoverActivities = () => {
         >
             <GridItem area={'search'}>
 
-                <Box position={'relative'} top={'50%'} left={'50%'} transform={'translate(-50%,-50%)'}
-                     textAlign={"center"}>
-                    <Button
-                        as="a"
-                        onClick={() => {
-                            navigate("/createevent");
-                        }}
-                        w={200}
-                        m={4}
-                        bg="teal"
-                    >
-                        <font size={5} color={"white"}>
-                            Create an event
-                        </font>
-                    </Button>
 
                 <Center>
 
