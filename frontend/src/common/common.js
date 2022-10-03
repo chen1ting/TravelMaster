8f2e5aa3136b7321125fd931b6b28c4d4b8b2b86
jmytkh <79437616+jmytkh@users.noreply.github.com>
2022-10-04 00:19:45 +0800

    Revert "Hid API Key"
    This reverts commit 1d042327a48a7544c024cbc1a7044078ffa0a6ba.

--- frontend/src/common/common.js
+++ frontend/src/common/common.js
@@ -1,6 +1,5 @@
 // common util functions
 import { validateToken } from "../api/api";
-import { apiKey } from "../api/apiKey"
 
 // use this function in a useEffect hook to trigger redirect to homepage
 // if a user has to be logged in first
@@ -17,5 +16,7 @@ const validSessionGuard = async (navigate, pathIfNotOk, pathIfOk) => {
     navigate(pathIfOk);
   }
 };
+
+const apiKey = "AIzaSyBH5ccwom9VK1HcDBWucl6t5h4B0AS5yDw";
 
 export { validSessionGuard, apiKey };
