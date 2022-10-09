1f8492e11e3bb091b80820aa8bf0d8e3fe417e65
tabbycat01 <geraldywy@gmail.com>
2022-10-10 00:16:49 +0800

    deployment stuff

--- backend/cmd/TravelMaster/main.go
+++ backend/cmd/TravelMaster/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/chen1ting/TravelMaster/internal/router"
 	"github.com/chen1ting/TravelMaster/internal/service"
 	"github.com/gin-gonic/gin"
@@ -11,5 +13,9 @@ func main() {
 	r := gin.Default()
 	router.InitRouting(r, s)
 
-	r.Run("0.0.0.0:8080")
+	if os.Getenv("APP_ENV") == "development" {
+		r.Run("0.0.0.0:8080")
+	} else {
+		r.RunTLS("0.0.0.0:8080", "server.pem", "key.unencrypted.pem")
+	}
 }
