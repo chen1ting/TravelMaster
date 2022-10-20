48af3ef885cc3c7a4c9014833419d889a789dda4
jmytkh <79437616+jmytkh@users.noreply.github.com>
2022-10-20 20:33:12 +0800

    Discover done

---
+++ frontend/cypress/e2e/createitinerary.cy.js
@@ -0,0 +1,31 @@
+
+describe('login', () => {
+  it('passes', () => {
+    cy.visit('https://172.21.148.164/')
+    //cy.visit('http://localhost:3000/')
+    cy.get('input').eq(0).type("Tom").should('have.value', 'Tom')
+    cy.get('input').eq(1).type("P@ssword").should('have.value', "P@ssword")
+    cy.get('button').eq(0).click()
+  })
+})
+
+/*
+describe('openlandingpage', () => {
+  it('passes', () => {
+    cy.visit('https://172.21.148.164/')
+    //cy.visit('http://localhost:3000/')
+  })
+})
+*/
+
+
+describe('welcomepagecheck', () => {
+  it('passes', () => {
+    cy.wait(2000) // wait for 2 seconds
+    cy.url().should('include', '/welcome') // => true
+    cy.contains('Hiking')
+    cy.get('button').contains("Plan my itinerary!").click()
+    cy.get('button').contains("Save changes made").click()
+  })
+})
+
--- frontend/cypress/e2e/discover.cy.js
+++ frontend/cypress/e2e/discover.cy.js
@@ -1,27 +1,60 @@
-/*
+
 describe('login', () => {
   it('passes', () => {
     cy.visit('https://172.21.148.164/')
     //cy.visit('http://localhost:3000/')
     cy.get('input').eq(0).type("Tom").should('have.value', 'Tom')
-    cy.get('input').eq(1).type("P@ssword", { delay: 200 }).should('have.value', "P@ssword")
+    cy.get('input').eq(1).type("P@ssword").should('have.value', "P@ssword")
+    cy.wait(1000)
     cy.get('button').eq(0).click()
   })
 })
-*/
 
+/*
 describe('openlandingpage', () => {
   it('passes', () => {
     cy.visit('https://172.21.148.164/')
     //cy.visit('http://localhost:3000/')
   })
 })
+*/
 
-describe('empty spec', () => {
+describe('opendiscoverpage', () => {
   it('passes', () => {
-    cy.url().should('include', '/welcome') // => true
-    cy.contains('Hiking')
-    cy.get('button').contains("Plan my itinerary!").click()
-    cy.get('button').contains("Save changes made").click()
+    cy.get('button').eq(0).click()
+    cy.url().should('include', '/discover') // => true
+  })
+})
+
+
+describe('searchbar', () => {
+  it('passes', () => {
+    cy.get('input').eq(0).type("Food").should('have.value', 'Food')
+    cy.get('button').contains('Search').click()
+    cy.wait(2000) // wait for 2 seconds
+  })
+})
+
+
+describe('openactivitypage', () => {
+  it('passes', () => {
+        cy.contains('Food').click()
+        cy.url().should('include', '/activity') // => true
+  })
+})
+
+describe('inactivereport', () => {
+  it('passes', () => {
+    cy.get('button').contains("Report inactive").click()
+    cy.get('button').contains("Remove inactive report").click()
+    cy.get('button').contains("Report inactive").click()
+    cy.get('button').contains("Remove inactive report").click()
+  })
+})
+
+
+describe('back', () => {
+  it('passes', () => {
+    cy.go('back')    
   })
 })
