9f1fab3b134a49952c0e6d576dcc38148c33a13b
jmytkh <79437616+jmytkh@users.noreply.github.com>
2022-10-20 14:05:34 +0800

    1

---
+++ frontend/cypress/e2e/login.cy.js
@@ -0,0 +1,9 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    //cy.visit('https://172.21.148.164/')
+    cy.visit('http://localhost:3000/')
+    cy.get('input').eq(0).type("Tom")
+    cy.get('input').eq(1).type("P@ssword")
+    cy.get('button').eq(0).click()
+  })
+})
