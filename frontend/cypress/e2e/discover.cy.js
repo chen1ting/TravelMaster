e85809646c9d6ef323677dcde20b294b37f81e38
jmytkh <79437616+jmytkh@users.noreply.github.com>
2022-10-22 19:43:13 +0800

    Update discover.cy.js

--- frontend/cypress/e2e/discover.cy.js
+++ frontend/cypress/e2e/discover.cy.js
@@ -5,7 +5,6 @@ describe('login', () => {
     //cy.visit('http://localhost:3000/')
     cy.get('input').eq(0).type("Tom").should('have.value', 'Tom')
     cy.get('input').eq(1).type("P@ssword").should('have.value', "P@ssword")
-    cy.wait(1000)
     cy.get('button').contains("Log").click()
     cy.wait(5000) // wait for 5 seconds
   })
@@ -75,8 +74,18 @@ describe('opendiscoverpage', () => {
 })
 
 
-describe('create new activity', () => {
+describe('create new activity2', () => {
   it('passes', () => {
     cy.get('button').contains("Create an activity").click()
+    cy.wait(2000)
+    cy.get('input').eq(1).click().type("Sentosa").should('have.value', 'Sentosa')
+    cy.get('textarea').click().type("Island of fun").should('have.value', 'Island of fun')
+    cy.get('input[type=file]').selectFile('./public/logo512.png')
+    cy.get('button').eq(16).click()
+    cy.get('button').eq(19).click()
+    cy.get('button').eq(20).click()
+    cy.get('button').eq(21).click()
+    //cy.get('input').eq(3).click().type("Sentosa, Singapore{enter}").should('have.value', 'Sentosa, Singapore')
+    cy.get('button').contains("Save")
   })
 })
