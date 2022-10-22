c29cda436603f3c65a59e51691ea065651a9b23b
jmytkh <79437616+jmytkh@users.noreply.github.com>
2022-10-22 10:33:44 +0800

    1

--- frontend/cypress/e2e/discover.cy.js
+++ frontend/cypress/e2e/discover.cy.js
@@ -1,4 +1,4 @@
-
+/*
 describe('login', () => {
   it('passes', () => {
     cy.visit('https://172.21.148.164/')
@@ -6,18 +6,18 @@ describe('login', () => {
     cy.get('input').eq(0).type("Tom").should('have.value', 'Tom')
     cy.get('input').eq(1).type("P@ssword").should('have.value', "P@ssword")
     cy.wait(1000)
-    cy.get('button').eq(0).click()
-  })
-})
-
-/*
-describe('openlandingpage', () => {
-  it('passes', () => {
-    cy.visit('https://172.21.148.164/')
-    //cy.visit('http://localhost:3000/')
+    cy.get('button').contains("Log").click()
   })
 })
 */
+
+describe('openlandingpage', () => {
+  it('passes', () => {
+    //cy.visit('https://172.21.148.164/')
+    cy.visit('http://localhost:3000/')
+  })
+})
+
 
 describe('opendiscoverpage', () => {
   it('passes', () => {
--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('signup', () => {
   it('passes', () => {
-    cy.visit('https://172.21.148.164/')
-    //cy.visit('http://localhost:3000/')
+    //cy.visit('https://172.21.148.164/')
+    cy.visit('http://localhost:3000/')
     cy.contains("Don't have an account? Sign Up Here").click()
     cy.url().should('include', '/Signup') // => true
     cy.get('input').eq(0).type("Tom").should('have.value', 'Tom')
@@ -15,11 +15,11 @@ describe('signup', () => {
 
 describe('login', () => {
   it('passes', () => {
-    cy.visit('https://172.21.148.164/')
-    //cy.visit('http://localhost:3000/')
+    //cy.visit('https://172.21.148.164/')
+    cy.visit('http://localhost:3000/')
     cy.get('input').eq(0).type("Tom").should('have.value', 'Tom')
     cy.get('input').eq(1).type("P@ssword", { delay: 200 }).should('have.value', "P@ssword")
-    cy.get('button').eq(0).click()
+    cy.get('button').contains("Log").click()
   })
 })
 
